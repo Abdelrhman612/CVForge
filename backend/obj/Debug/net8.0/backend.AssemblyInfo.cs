724213d7ae2969294693a9c5447c18f0c4f94c22
Abdelrahman <abdelrahman.ayman.tech@gmail.com>
2026-03-10 21:04:31 +0200

    first commit

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b4689e7c0f59fdde8caa0ca023303e0ca40150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0cd9079d30fa57ca07f7d35772c53a84bc9bb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
